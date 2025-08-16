551d76daaa8514871eb2f651b2825c62c72e8f2b
Mamta <mamtarana011@gmail.com>
2025-08-17 01:43:18 +0530

    new changes cors related

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());//middleware
+
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://beamish-meringue-1675ff.netlify.app/'
+}));
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
